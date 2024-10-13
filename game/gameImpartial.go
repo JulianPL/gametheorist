9dfb0893c8f84d18ad226088c8fffc8d569f8b37
Julian Pape-Lange <j.pape-lange@gmx.de>
2024-10-13 13:27:50 +0200

    :sparkles: Add game "the white knight"

--- game/gameImpartial.go
+++ game/gameImpartial.go
@@ -12,3 +12,33 @@ func Nim(stacks collection.Set[number.Nimber]) Impartial {
 	}
 	return value
 }
+
+func TheWhiteKnight(left, right int) Impartial {
+	a := make([][]Impartial, left+right+2)
+	for i := range a {
+		a[i] = make([]Impartial, left+right+2)
+	}
+	for sum := 2; sum <= left+right+2; sum++ {
+		for l := 1; l <= left+right+1; l++ {
+			r := sum - l
+			if r <= 0 {
+				break
+			}
+			options := collection.MakeSet[Impartial]()
+			if l-2 > 0 && r-1 > 0 {
+				options.Add(a[l-2][r-1])
+			}
+			if l-2 > 0 && r+1 > 0 {
+				options.Add(a[l-2][r+1])
+			}
+			if l-1 > 0 && r-2 > 0 {
+				options.Add(a[l-1][r-2])
+			}
+			if l+1 > 0 && r-2 > 0 {
+				options.Add(a[l+1][r-2])
+			}
+			a[l][r] = MinimalExcluded(options)
+		}
+	}
+	return a[left][right]
+}
--- game/gameImpartial_test.go
+++ game/gameImpartial_test.go
@@ -23,3 +23,53 @@ func Test_Nim(t *testing.T) {
 		t.Errorf("NimValue was incorrect, got: %v, want: %v.", sumActual, sumExpected)
 	}
 }
+
+func Test_TheWhiteKnight(t *testing.T) {
+	left := 1
+	right := 2
+	gameActual := TheWhiteKnight(left, right)
+	gameExpected := MakeGameImpartial("0")
+	if gameActual != gameExpected {
+		t.Errorf("TheWhiteKnight was incorrect, got: %v, want: %v.", gameActual, gameExpected)
+	}
+
+	left = 5
+	right = 4
+	gameActual = TheWhiteKnight(left, right)
+	gameExpected = MakeGameImpartial("4")
+	if gameActual != gameExpected {
+		t.Errorf("TheWhiteKnight was incorrect, got: %v, want: %v.", gameActual, gameExpected)
+	}
+
+	left = 5
+	right = 3
+	gameActual = TheWhiteKnight(left, right)
+	gameExpected = MakeGameImpartial("3")
+	if gameActual != gameExpected {
+		t.Errorf("TheWhiteKnight was incorrect, got: %v, want: %v.", gameActual, gameExpected)
+	}
+
+	left = 9
+	right = 10
+	gameActual = TheWhiteKnight(left, right)
+	gameExpected = MakeGameImpartial("0")
+	if gameActual != gameExpected {
+		t.Errorf("TheWhiteKnight was incorrect, got: %v, want: %v.", gameActual, gameExpected)
+	}
+
+	left = 14
+	right = 7
+	gameActual = TheWhiteKnight(left, right)
+	gameExpected = MakeGameImpartial("2")
+	if gameActual != gameExpected {
+		t.Errorf("TheWhiteKnight was incorrect, got: %v, want: %v.", gameActual, gameExpected)
+	}
+
+	left = 15
+	right = 9
+	gameActual = TheWhiteKnight(left, right)
+	gameExpected = MakeGameImpartial("1")
+	if gameActual != gameExpected {
+		t.Errorf("TheWhiteKnight was incorrect, got: %v, want: %v.", gameActual, gameExpected)
+	}
+}
